225301316f0d7a5e7b7c499ff035380413d32679
Mudakkir afridi <mudakkirafridi@gmail.com>
2023-12-25 22:32:27 +0500

    veriables and datatypes complete

---
+++ veriables.dart
@@ -0,0 +1,18 @@
+void main() {
+  int number = 12;
+  double num = 12.5;
+  String name = 'Mudakir afridi';
+  bool isMudakirAvailable = true;
+  var age = 12;
+
+  // in var we can store any type of data or update data with same data type
+
+  //lists
+  List<String> family = [
+    "mudakir",
+    "khan",
+    "afridi",
+  ];
+  print(family[2]);
+  // this is the list of type String we can store only string data
+}
